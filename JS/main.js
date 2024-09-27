0791f37161fba59b1329ed00bf4fde3d9876604a
Abdelrahmann-Ahmedd <abdelrahman.w164@gmail.com>
2024-09-27 11:59:37 +0300

    first commit

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,8 @@
-const menuIcon = document.querySelector('.menuIcon');
-const nav = document.querySelector('nav');
-const navLeft = document.querySelector('nav .leftSide');
-const navRight = document.querySelector('nav .rightSide');
-const navLinks = document.querySelector('nav .rightSide ul');
+var menuIcon = document.querySelector('.menuIcon');
+var navRight = document.querySelector('nav .rightSide');
+var nav = document.querySelector("nav")
 
 menuIcon.addEventListener('click', () => {
-    nav.classList.toggle('nav');
-    navLeft.classList.toggle('leftSide');
-    navLeft.classList.toggle('nav-leftSide');
-    navRight.classList.toggle('rightSide');
-    navRight.classList.toggle('nav-rightSide');
-    navLinks.classList.toggle('nav-rightSide-ul');
+    navRight.classList.toggle("hide");
+    nav.classList.toggle("dark");
 });
